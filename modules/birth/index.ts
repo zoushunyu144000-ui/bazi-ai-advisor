a08246988cce6b76efa161e567f5d3f96a0026ea
zoushunyu144000-ui <zoushunyu144000@gmail.com>
2026-08-18 10:37:16 +0800

    birth: export provider adapters

--- modules/birth/index.ts
+++ modules/birth/index.ts
@@ -1,5 +1,6 @@
 export * from "./errors.ts";
 export * from "./location.ts";
 export * from "./normalize.ts";
+export * from "./providers/index.ts";
 export * from "./timezone.ts";
 export * from "./types.ts";
