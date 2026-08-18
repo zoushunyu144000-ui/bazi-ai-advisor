b4a12b18dca99ec953039462af03926260c727df
zoushunyu144000-ui <zoushunyu144000@gmail.com>
2026-08-18 11:16:35 +0800

    bazi: export shared result contract

--- modules/bazi/index.ts
+++ modules/bazi/index.ts
@@ -2,5 +2,13 @@
 export { calculateBazi } from './engine';
 export { ENGINE_VERSION, MAPPING_VERSION, RULE_PROFILE_VERSION } from './constants';
 export { solarTermInstantMs } from './adapters/tyme4ts-adapter';
-export type { BaziEngineResult, BaziLuckStructure, BaziRelation } from './types';
-export type { BaziCalculationMetadata, BaziChart, BaziDerivedFeatures, BirthProfile } from '../../types/domain';
+export type {
+  BaziCalculationContext,
+  BaziCalculationMetadata,
+  BaziCalculationResult,
+  BaziChart,
+  BaziDerivedFeatures,
+  BaziLuckStructure,
+  BaziRelation,
+  BirthProfile,
+} from '../../types/domain';
