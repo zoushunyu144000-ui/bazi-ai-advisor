24de59deb2e4f714ba050bf832b780391edbcb44
zoushunyu144000-ui <zoushunyu144000@gmail.com>
2026-08-18 11:16:07 +0800

    bazi: use shared relation contract

--- modules/bazi/relations.ts
+++ modules/bazi/relations.ts
@@ -1,6 +1,5 @@
-import type { BaziChart, EarthlyBranch, HeavenlyStem } from '../../types/domain';
+import type { BaziChart, BaziRelation, EarthlyBranch, HeavenlyStem } from '../../types/domain';
 import { PILLAR_NAMES } from './constants';
-import type { BaziRelation, BaziRelationKind } from './types';
 
 const STEM_COMBINATIONS: [HeavenlyStem,HeavenlyStem][]=[['jia','ji'],['yi','geng'],['bing','xin'],['ding','ren'],['wu','gui']];
 const BRANCH_COMBINATIONS:[EarthlyBranch,EarthlyBranch][]=[['zi','chou'],['yin','hai'],['mao','xu'],['chen','you'],['si','shen'],['wu','wei']];
@@ -12,13 +11,12 @@ function pairMatch<T extends string>(pairs:[T,T][],a:T,b:T):boolean { return pai
 export function calculateRelations(chart:BaziChart):BaziRelation[] {
   const entries=PILLAR_NAMES.flatMap(name=>{const p=chart.pillars[name];return p?[{name,p}]:[];});
   const out:BaziRelation[]=[];
-  const add=(kind:BaziRelationKind,leftPillar:typeof PILLAR_NAMES[number],rightPillar:typeof PILLAR_NAMES[number],left:string,right:string)=>out.push({kind,leftPillar,rightPillar,left,right});
   for(let i=0;i<entries.length;i++) for(let j=i+1;j<entries.length;j++) {
     const a=entries[i],b=entries[j];
-    if(pairMatch(STEM_COMBINATIONS,a.p.stem,b.p.stem)) add('stem_combination',a.name,b.name,a.p.stem,b.p.stem);
-    if(pairMatch(BRANCH_COMBINATIONS,a.p.branch,b.p.branch)) add('branch_combination',a.name,b.name,a.p.branch,b.p.branch);
-    if(pairMatch(BRANCH_CLASHES,a.p.branch,b.p.branch)) add('branch_clash',a.name,b.name,a.p.branch,b.p.branch);
-    if(pairMatch(BRANCH_HARMS,a.p.branch,b.p.branch)) add('branch_harm',a.name,b.name,a.p.branch,b.p.branch);
+    if(pairMatch(STEM_COMBINATIONS,a.p.stem,b.p.stem)) out.push({kind:'stem_combination',leftPillar:a.name,rightPillar:b.name,left:a.p.stem,right:b.p.stem});
+    if(pairMatch(BRANCH_COMBINATIONS,a.p.branch,b.p.branch)) out.push({kind:'branch_combination',leftPillar:a.name,rightPillar:b.name,left:a.p.branch,right:b.p.branch});
+    if(pairMatch(BRANCH_CLASHES,a.p.branch,b.p.branch)) out.push({kind:'branch_clash',leftPillar:a.name,rightPillar:b.name,left:a.p.branch,right:b.p.branch});
+    if(pairMatch(BRANCH_HARMS,a.p.branch,b.p.branch)) out.push({kind:'branch_harm',leftPillar:a.name,rightPillar:b.name,left:a.p.branch,right:b.p.branch});
   }
   return out;
 }
